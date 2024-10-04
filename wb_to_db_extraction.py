943fe9a38bc8eacf7ad0c2f41c791f03c26c9984
MikheevKirill <mixeevk@gmail.com>
2024-10-04 23:44:54 +0400

    Add files via upload
    Версия с возможностью ввести поисковый запрос и получать информацию уже по нему

--- wb_to_db_extraction.py
+++ wb_to_db_extraction.py
@@ -3,13 +3,15 @@ import psycopg2
 import requests
 import datetime
 import time
+import urllib.parse
 
 
 
-def extract_all_pages():
+def extract_all_pages(string_to_url):
+    string_to_url = urllib.parse.quote(string_to_url)
     values = []
     for i in range(1, 101):
-        url = 'https://search.wb.ru/exactmatch/ru/male/v7/search?ab_testing=false&appType=1&curr=rub&dest=-535680&page=' + str(i) + '&query=%D0%B1%D1%80%D1%8E%D0%BA%D0%B8%20%D0%BC%D1%83%D0%B6%D1%81%D0%BA%D0%B8%D0%B5&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false&uclusters=0'
+        url = 'https://search.wb.ru/exactmatch/ru/male/v7/search?ab_testing=false&appType=1&curr=rub&dest=-535680&page=' + str(i) + '&query=' + string_to_url + '&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false&uclusters=0'
         headers = {
             'Authorization': f'Bearer {'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mjc1NjM2MzcsInZlcnNpb24iOjIsInVzZXIiOiI4NDg5MjI0MyIsInNoYXJkX2tleSI6IjIiLCJjbGllbnRfaWQiOiJ3YiIsInNlc3Npb25faWQiOiI3ODQzZTA1NWI4MDY0YmVjOGQ0ZTk2ZTMyMWI2ODY2YyIsInVzZXJfcmVnaXN0cmF0aW9uX2R0IjoxNjc0MDQ3ODQyLCJ2YWxpZGF0aW9uX2tleSI6ImUzNmI2ZjY1OWJjYzYzMTk0Nzg3NjYwOTA4MDgyZTRkNTAyNmE1MjE5MTQ0ZDMxMTEyMjVlMjk0MjkwNjI5ODUiLCJwaG9uZSI6ImRXMVJuN2xSVDdJeUFVZ1NaYjB5RkE9PSJ9.A3s9h32e_nlxYaYXuZZYgjKYLa44v2tJEFYK0WfynuJtbyPLhqTwqJX01ZcQ1IdfXkpR2PBztDiAn9nGbkovFzrT1NDNKC-vSZ-l1Qu6mMTeHOs5mw1iVljYk5nQF-Hi89Lgu7FgLVuqgUwx_mHCewBtzyyf_rGHEiNj0y0eFxH5v5Kufd5n11fzDoSihZbaxjSmEhv-KeJdLsj46Rx6kzuMrR_74Bp7NbwwoNmr3aPFFCcmZPSXV50-qlELLhlQa21MeXtyJ8smQf5obLxrDVgpgWOokBfytxkjHnpGNQAnB2MMHJtlAwNrojT0qbjMRhNzjx-uJo6B6BYYbJrHew'}',
         }
@@ -107,7 +109,7 @@ def main_task():
 
     counter = 0
 
-    extracted_pages = extract_all_pages()
+    extracted_pages = extract_all_pages(items_to_search)
 
     extracted_data = find_values_by_key(extracted_pages)
 
@@ -123,7 +125,8 @@ def main_task():
     print('Строк изменено: ', counter)
 
 if __name__ == '__main__':
+    items_to_search = str(input())
     print('Start at', datetime.datetime.now())
-    schedule.every(8).minutes.do(main_task)
+    schedule.every(1).minutes.do(main_task)
     while True:
         schedule.run_pending()
